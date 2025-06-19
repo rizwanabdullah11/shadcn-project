90a9c476689dbb400f0f3df024b8c6ce0b3f70e8
Rizwan <rizwan.abdullah193@gmail.com>
2025-06-19 06:49:00 -0700

    add- things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-
-export default function Home() {
+const Homepage = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+      <div className="bg-background-foreground p-4 rounded-lg border-2 lg:col-span-3">Test</div>
+      <div className="bg-background-foreground p-4 rounded-lg border-2">Test</div>
+      <div className="bg-background-foreground p-4 rounded-lg border-2">Test</div>
+      <div className="bg-background-foreground p-4 rounded-lg border-2">Test</div>
+      <div className="bg-background-foreground p-4 rounded-lg border-2 lg:col-span-3">Test</div>
+      <div className="bg-background-foreground p-4 rounded-lg border-2">Test</div> 
     </div>
   );
 }
+export default Homepage;
